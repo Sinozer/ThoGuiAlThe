c2c385de11797eb538d6cd7a46f07dbc436f33ce
Guillaume Blackburn <guillaume.blackburn1@gmail.com>
2023-11-14 09:57:13 +0100

    feat: Init server

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -1,4 +1,96 @@
 int main()
 {
+    static constexpr char PORT[5] = "6969";
+
+    SOCKET serverSocket = INVALID_SOCKET, listenSocket = INVALID_SOCKET;
+
+    addrinfo* result = NULL;
+    addrinfo hints{};
+
+    // Initialize Winsock
+    WSADATA wsaData;
+    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0)
+    {
+        LOG("WSAStartup failed with error: " << iResult);
+        return 1;
+    }
+    else
+        LOG("WSAStartup success");
+
+    // Resolve address and port
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_flags = AI_PASSIVE; // For wildcard IP address
+
+
+    iResult = getaddrinfo("localhost", PORT, &hints, &result);
+
+    if (iResult != 0)
+    {
+        LOG("getaddrinfo failed with error: " << iResult);
+        WSACleanup();
+        return 1;
+    }
+    else
+        LOG("getaddrinfo success");
+
+    // Socket creation
+    listenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (listenSocket == INVALID_SOCKET)
+    {
+        LOG("socket failed with error: " << WSAGetLastError());
+        freeaddrinfo(result);
+        WSACleanup();
+        return 1;
+    }
+    else
+        LOG("socket success");
+
+    // Bind socket to listen to TCP requests on the given address and port
+    iResult = bind(listenSocket, result->ai_addr, (int)result->ai_addrlen);
+    if (iResult == SOCKET_ERROR)
+    {
+        LOG("bind failed with error: " << WSAGetLastError());
+        freeaddrinfo(result);
+        closesocket(listenSocket);
+        WSACleanup();
+        return 1;
+    }
+    else
+        LOG("bind success");
+    
+    freeaddrinfo(result);
+
+    iResult = listen(listenSocket, SOMAXCONN);
+    if (iResult == SOCKET_ERROR)
+    {
+        LOG("listen failed with error: " << WSAGetLastError());
+        closesocket(listenSocket);
+        WSACleanup();
+        return 1;
+    }
+    else
+        LOG("listen success");
+
+    // Accept a client socket
+    serverSocket = accept(listenSocket, NULL, NULL);
+    if (serverSocket == INVALID_SOCKET)
+    {
+        LOG("accept failed with error: " << WSAGetLastError());
+        closesocket(listenSocket);
+        WSACleanup();
+        return 1;
+    }
+    else
+        LOG("accept success");
+
+    // No longer need server socket
+    closesocket(listenSocket);
+
+    LOG("Server is running");
+
+    WSACleanup();
     return 0;
 }
--- Server/src/stdafx.h
+++ Server/src/stdafx.h
@@ -1,6 +1,14 @@
+
 #include <iostream>
 #include <string>
 
 #include <WinSock2.h>
 #include <Winerror.h>
 #include <WS2tcpip.h>
+
+
+#if defined(_DEBUG) || defined(DEBUG) && !defined(LOG)
+#define LOG(x) std::cout << x << '\n'
+#else
+#define LOG(x)
+#endif
