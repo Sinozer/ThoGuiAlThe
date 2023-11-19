0cfb064e21c367a941faffbc3bfbd5fff80dd36f
Guillaume Blackburn <guillaume.blackburn1@gmail.com>
2023-11-19 22:42:55 +0100

    feat: can insert custom values in the html file

--- Server/src/Network/Http/RequestHandlers/HomeRequestHandler.cpp
+++ Server/src/Network/Http/RequestHandlers/HomeRequestHandler.cpp
@@ -17,8 +17,26 @@ std::string HomeRequestHandler::HandleHttpRequest(std::unordered_map<std::string
 
 std::string HomeMethodHandler::BuildResponse(std::unordered_map<std::string, std::string>& params) const
 {
+	int customValues[3][3] = {
+	{00, 01, 02},
+	{10, 11, 12},
+	{20, 21, 22}
+	};
+
 	std::ifstream file("assets/index.html");
 	std::string fileContent((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+
+	// Replace placeholders with actual values using regex
+	for (int i = 0; i < 3; ++i)
+	{
+		for (int j = 0; j < 3; ++j)
+		{
+			// Generate the placeholder string that can be found in the html file
+			std::string placeholder = "%VALUE_" + std::to_string(i) + std::to_string(j) + "%";
+			std::regex pattern(placeholder);
+			fileContent = std::regex_replace(fileContent, pattern, std::to_string(customValues[i][j]));
+		}
+	}
 
 	std::string response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: " + std::to_string(fileContent.size()) + "\r\n\r\n" + fileContent;
 	return response;
--- Server/src/stdafx.h
+++ Server/src/stdafx.h
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <memory>
 #include <conio.h>
+#include <regex>
 /* ################# STANDARD ################# */
 
 /* ##################  WINSOCK  ################## */
