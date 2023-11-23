152e480633c1437d68d9eec849f33fd1d764b304
Théo <tdasilva@gaming.tech>
2023-11-23 21:34:31 +0100

    Updated the ui of the profilestate

--- ThoGuiAlThe/src/App/State/List/Profile/ProfileState.cpp
+++ ThoGuiAlThe/src/App/State/List/Profile/ProfileState.cpp
@@ -40,6 +40,8 @@ void ProfileState::InitUi()
 
 #pragma region PROFILE_PICTURE
 	auto* picture = m_UiManager.AddImage("PLAYER_PICTURE", playerDisplayData.profilePicturePath);
+	picture->SetOutlineColor(sf::Color(playerDisplayData.color[0], playerDisplayData.color[1], playerDisplayData.color[2], playerDisplayData.color[3]));
+	picture->SetOutlineThickness(4.f);
 	picture->setPosition(
 		WINDOW_SCREEN_WIDTH / 2.f - picture->getGlobalBounds().width / 2.f,
 		pName->getPosition().y + pName->getGlobalBounds().getSize().y * 1.25f
